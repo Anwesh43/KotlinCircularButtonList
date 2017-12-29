e5180c1bc0d846989b4b2ee53ef3230758aff358
Anwesh43 <anweshthecool0@gmail.com>
2017-12-30 02:23:32 +0530

    adding view in Activity

--- app/src/main/java/ui/anwesome/com/kotlincircularbuttonlist/MainActivity.kt
+++ app/src/main/java/ui/anwesome/com/kotlincircularbuttonlist/MainActivity.kt
@@ -2,11 +2,17 @@ package ui.anwesome.com.kotlincircularbuttonlist
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import ui.anwesome.com.circularbuttonlist.CircularButtonListView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val view = CircularButtonListView.create(this)
+        view.addText("Hello")
+        view.addText("More")
+        view.addText("Store")
+        view.addText("Please")
+        view.addToParent(this)
     }
 }
