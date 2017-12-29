b9c25bf6c3c631ca065c9c4ed62f361a35727416
Anwesh43 <anweshthecool0@gmail.com>
2017-12-30 01:07:06 +0530

    using State in ViewAnimatorQueue

--- circularbuttonlist/src/main/java/ui/anwesome/com/circularbuttonlist/ViewAnimatorQueue.kt
+++ circularbuttonlist/src/main/java/ui/anwesome/com/circularbuttonlist/ViewAnimatorQueue.kt
@@ -8,12 +8,18 @@ import java.util.concurrent.ConcurrentLinkedQueue
  */
 class ViewAnimatorQueue(var view:View) {
     var animated:Boolean = false
+    val state = State()
     val animations:ConcurrentLinkedQueue<(Float)->Unit> = ConcurrentLinkedQueue()
     fun addAnimation(cb:(Float)->Unit) {
         animations.add(cb)
     }
     fun animate() {
         if(animated) {
+            state.update({scale ->
+                animations.at(0)?.invoke(scale)
+            },{
+                animated = false
+            })
             try {
                 Thread.sleep(50)
                 view.invalidate()
@@ -34,8 +40,18 @@ data class State(var scale:Float = 0f) {
         scale += 0.1f
         updatecb(scale)
         if(scale > 1) {
-            scale = 1f
+            scale = 0f
             stopcb()
         }
     }
+}
+fun ConcurrentLinkedQueue<(Float)->Unit>.at(i:Int):((Float)->Unit)? {
+    var index = 0
+    this.forEach {
+        if(index == i) {
+            return it
+        }
+        index++
+    }
+    return null
 }
